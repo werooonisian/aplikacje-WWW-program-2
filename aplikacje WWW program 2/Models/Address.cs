41f502da63c810b08db7b324d5a7870105e6ea98
Weronika Rogozińska <weruka2000@wp.pl>
2021-03-15 19:27:20 +0100

    zadanie 2

---
+++ aplikacje WWW program 2/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace aplikacje_WWW_program_2.Models
+{
+    public class Address 
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ aplikacje WWW program 2/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using aplikacje_WWW_program_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aplikacje_WWW_program_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Address Address { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+
+        }   
+    }
+}
